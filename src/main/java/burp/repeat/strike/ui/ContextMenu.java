84468556d36967307d9692c3636c258eb74acf10
Gareth Heyes <gazheyes@gmail.com>
2025-04-29 15:11:54 +0100

    Fixed context menu to work only on repeater requests.

--- src/main/java/burp/repeat/strike/ui/ContextMenu.java
+++ src/main/java/burp/repeat/strike/ui/ContextMenu.java
@@ -1,9 +1,11 @@
 package burp.repeat.strike.ui;
 
+import burp.api.montoya.core.ToolType;
 import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.http.message.responses.HttpResponse;
 import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
 import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
+import burp.api.montoya.ui.contextmenu.InvocationType;
 import burp.repeat.strike.ai.VulnerabilityAnalysis;
 import burp.repeat.strike.settings.Settings;
 
@@ -16,18 +18,20 @@ public class ContextMenu implements ContextMenuItemsProvider {
     public java.util.List<Component> provideMenuItems(ContextMenuEvent event)
     {
         java.util.List<Component> menuItemList = new ArrayList<>();
-        JMenuItem runRepeatStrike = new JMenuItem("Run Repeat Strike");
-        runRepeatStrike.addActionListener(e -> {
-            if(event.messageEditorRequestResponse().isPresent()) {
-                HttpRequest req = event.messageEditorRequestResponse().get().requestResponse().request();
-                HttpResponse resp = event.messageEditorRequestResponse().get().requestResponse().response();
-                if (req == null || resp == null) {
-                    return;
+        if(event.messageEditorRequestResponse().isPresent() && event.invocationType() == InvocationType.MESSAGE_EDITOR_REQUEST && event.isFromTool(ToolType.REPEATER)) {
+            JMenuItem runRepeatStrike = new JMenuItem("Run Repeat Strike");
+            runRepeatStrike.addActionListener(e -> {
+                if(event.messageEditorRequestResponse().isPresent()) {
+                    HttpRequest req = event.messageEditorRequestResponse().get().requestResponse().request();
+                    HttpResponse resp = event.messageEditorRequestResponse().get().requestResponse().response();
+                    if (req == null || resp == null) {
+                        return;
+                    }
+                    VulnerabilityAnalysis.check(req, resp);
                 }
-                VulnerabilityAnalysis.check(req, resp);
-            }
-        });
-        menuItemList.add(runRepeatStrike);
+            });
+            menuItemList.add(runRepeatStrike);
+        }
         JMenuItem settings = new JMenuItem("Settings");
         settings.addActionListener(e -> Settings.showSettingsWindow());
         menuItemList.add(settings);
